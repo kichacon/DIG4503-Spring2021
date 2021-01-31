32fffae63d8f1ba0311397e1c10507d9afc9c593
kichacon <kiarachacon8@yahoo.com>
2021-01-31 08:29:25 -0500

    lab2

--- lab2/Fetch.js
+++ lab2/Fetch.js
@@ -1,3 +1,6 @@
+import axios from 'axios';
+import chalk from 'chalk';
+
 class Fetch {
     pokemon = "";
     color = "";
@@ -5,3 +8,29 @@ class Fetch {
     constructor(pokemon, color) {
         this.pokemon = pokemon;
         this.color = color;
+
+    }
+
+    fetch() {
+    
+       // Fetch this URL
+axios('https://pokeapi.co/api/v2/pokemon/ditto')
+    // And then...
+    .then(function (response) {
+        // Use the 'data'
+        const pokemon = response.data;
+
+        // In this example, the above URL will return an object
+        //  with data on the Pokemon requested
+        console.log(chalk.hex("This is a " + pokemon.name + " and its ID is " + pokemon.id));
+    })
+    .catch(function (error) {
+        // handle error
+        console.log(chalk.red("Error: " + error));
+    });
+
+}
+}
+
+
+const c = new Fetch();
---
+++ lab2/index.js
@@ -0,0 +1,9 @@
+import Fetch from './Fetch.js';
+
+class Display extends Fetch {
+    constructor() {
+        super("Pikachu", "Green");
+    }
+}
+
+const c2 = new Display();
