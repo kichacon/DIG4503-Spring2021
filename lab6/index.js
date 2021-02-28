a940f217264fbc8a3a5617973d5b4d0d006839b6
kichacon <kiarachacon8@yahoo.com>
2021-02-28 08:39:08 -0500

    lab6

---
+++ lab6/index.js
@@ -0,0 +1,17 @@
+import Express from 'express';
+
+const App = Express();
+const port = 45030;
+
+const person = {
+    name: "Kiara",
+    color: "Purple"
+};
+
+App.get("/", function(req, res) {
+    res.json(person);
+});
+
+App.listen(port, function() {
+    console.log("Hello World");
+});
