64ede182ffc4bd067917d7105a2692d89a00d3ca
kichacon <kiarachacon8@yahoo.com>
2021-03-28 10:49:48 -0400

    lab9

--- lab9/index.js
+++ lab9/index.js
@@ -1,13 +1,13 @@
 import MongoClient from 'mongodb';
 
-const URL = "";
+const URL = "mongodb+srv://KiaraChacon:jvSTx5EKRKWgmudW@cluster0.yuzwq.mongodb.net";
 
 MongoClient.connect(URL, { useUnifiedTopology: true})
 .then(connection => {
-    let database = connection.db("sample_restaurants");
-    let collection = database.collection("restaurants");
+    let database = connection.db("sample_airbnb");
+    let collection = database.collection("listingsAndReviews");
 
-    let cursor = collection.find({"grades.grade": "A", cusine: "Bakery"});
+    let cursor = collection.find({review_scores_rating: {$gte: 99}, beds: {$gte: 5}, price: {$lte: 200}});
 
     cursor.forEach(document => {
         console.log(document.title);
