cac21724755f93d08ae713375a173a65fd26b390
kichacon <kiarachacon8@yahoo.com>
2021-04-12 05:17:51 -0400

    lab9

--- lab9/index.js
+++ lab9/index.js
@@ -1,21 +1,24 @@
+
 import MongoClient from 'mongodb';
 
 const URL = "mongodb+srv://KiaraChacon:jvSTx5EKRKWgmudW@cluster0.yuzwq.mongodb.net";
 
-MongoClient.connect(URL, { useUnifiedTopology: true})
+MongoClient.connect(URL, { useUnifiedTopology: true })
 .then(connection => {
+    
     let database = connection.db("sample_airbnb");
     let collection = database.collection("listingsAndReviews");
 
-    let cursor = collection.find({review_scores_rating: {$gte: 99}, beds: {$gte: 5}, price: {$lte: 200}});
+    collection.findOne({minimum_nights: { $eq: "2" } })
+    .then((results) => {
+        
+        // Do something with the results!
 
-    cursor.forEach(document => {
-        console.log(document.title);
-    }, () => {
+        // Close the connection!
         connection.close();
-    });
-
+    })
+   
 })
 .catch(error => {
-    console.log("Error" + error);
+    console.log("Error: " + error);
 });
